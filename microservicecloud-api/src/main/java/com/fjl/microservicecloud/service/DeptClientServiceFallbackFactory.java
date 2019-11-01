ab91c39942b21e11366730dc84a7a6d2d7cd7e98
fengjialuo <Jialuo_Feng@foxmail.com>
2019-11-01 20:18:24 +0800

    feat:API公共模块和部门Entity步骤

---
+++ microservicecloud-api/src/main/java/com/fjl/microservicecloud/service/DeptClientServiceFallbackFactory.java
@@ -0,0 +1,31 @@
+package com.fjl.microservicecloud.service;
+
+import com.fjl.microservicecloud.entities.Dept;
+import feign.hystrix.FallbackFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component // 不要忘记添加，不要忘记添加
+public class DeptClientServiceFallbackFactory implements FallbackFactory<com.fjl.microservicecloud.service.DeptClientService> {
+    @Override
+    public com.fjl.microservicecloud.service.DeptClientService create(Throwable throwable) {
+        return new com.fjl.microservicecloud.service.DeptClientService() {
+            @Override
+            public Dept get(long id) {
+                return new Dept().setDeptno(id).setDname("该ID：" + id + "没有没有对应的信息,Consumer客户端提供的降级信息,此刻服务Provider已经关闭")
+                        .setDb_source("no this database in MySQL");
+            }
+
+            @Override
+            public List<Dept> list() {
+                return null;
+            }
+
+            @Override
+            public boolean add(Dept dept) {
+                return false;
+            }
+        };
+    }
+}
